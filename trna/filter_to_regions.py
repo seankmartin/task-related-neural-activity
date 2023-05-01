6a328608d7c37490305b77e08e48ea5cf075e7d1
Sean Martin <seankieran.m@hotmail.com>
2023-05-01 21:12:25 +0100

    Fix grabbing regions

--- trna/filter_to_regions.py
+++ trna/filter_to_regions.py
@@ -4,16 +4,13 @@ import ast
 def filter_allen_table(allen_table, brain_regions):
     def check_brain_regions(x):
         x = ast.literal_eval(x)
-        all_matches = []
-        for brain_region_set in brain_regions:
-            matches = []
-            for br in brain_region_set:
-                if isinstance(br, str):
-                    matches.append(br in x)
-                else:
-                    matches.append(any([b in x for b in br]))
-            all_matches.append(all(matches))
-        return any(all_matches)
+        matches = []
+        for br in brain_regions:
+            if isinstance(br, str):
+                matches.append(br in x)
+            else:
+                matches.append(any([b in x for b in br]))
+        return all(matches)
 
     desired_sessions = allen_table[
         allen_table["structure_acronyms"].apply(check_brain_regions)
@@ -27,16 +24,13 @@ def filter_allen_table(allen_table, brain_regions):
 def filter_ibl_table(ibl_table, region_table, brain_regions):
     def check_brain_regions(x):
         x = ast.literal_eval(x)
-        all_matches = []
-        for brain_region_set in brain_regions:
-            matches = []
-            for br in brain_region_set:
-                if isinstance(br, str):
-                    matches.append(br in x)
-                else:
-                    matches.append(any([b in x for b in br]))
-            all_matches.append(all(matches))
-        return any(all_matches)
+        matches = []
+        for br in brain_regions:
+            if isinstance(br, str):
+                matches.append(br in x)
+            else:
+                matches.append(any([b in x for b in br]))
+        return all(matches)
 
     desired_sessions = region_table[region_table["regions"].apply(check_brain_regions)]
     filtered_table = ibl_table.loc[
